d022e37319ee56f2f27b53093a410e5ceac5c5ca
Amina Islam Katha <140314069+aminakatha@users.noreply.github.com>
2023-07-30 14:45:50 +0600

    Add files via upload

---
+++ katha.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout<<"Weelcome to the wonderful world of C++!!!\n";
+    cout<<"Lab1 \t";
+    cout<<"\n END\t PROGRAM\t BYE";
+    cout<<"Welcome in yarmouk university\rHellooo";
+
+}
